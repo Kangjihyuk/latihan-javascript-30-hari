22a3297b9628cafc4e8c11a2cc07eab4303ce6a4
khusni mubarok <husnirqw@gmail.com>
2024-02-25 19:29:06 +0700

    latihan hari ke 2

---
+++ javascript days 2/main.js
@@ -0,0 +1,55 @@
+//soal 1
+let tantangan = "30 Hari javascript";
+//soal 2
+console.log("hello world");
+//soal 3
+console.log(tantangan.length);
+//soal 4
+console.log(tantangan.toUpperCase());
+//soal 5
+console.log(tantangan.toLowerCase());
+//soal 6
+console.log(tantangan.substring(0, 2));
+//soal 7
+console.log(tantangan.slice(3));
+//soal 8
+console.log(tantangan.includes("script"));
+//soal 9
+console.log(tantangan.split(" "));
+//soal 10
+console.log(tantangan.split(""));
+//soal 11
+let js = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+console.log(js.split(" "));
+//soal 12
+console.log(tantangan.replace("javascript", "phyton"));
+//soal 13
+console.log(tantangan.charAt(15));
+//soal 14
+console.log(tantangan.charCodeAt(9));
+//soal 15
+console.log(tantangan.indexOf("a"));
+//soal 16
+console.log(tantangan.lastIndexOf("a"));
+//soal 17
+const because =
+  "You cannot end a sentence with because because because is a conjunction";
+console.log(because.indexOf("because"));
+//soal 18
+console.log(because.lastIndexOf("beacuse"))
+//soal 19
+console.log(because.search("because"))
+//soal 20
+console.log(tantangan.trim())
+//soal 21
+console.log(tantangan.startsWith(""))
+//soal 22
+console.log(tantangan.endsWith(""))
+//soal 23
+console.log(tantangan.match("a"))
+//soal 24
+const concat = "30 Days of"
+const concat2 = "javascript"
+console.log(`${concat} ${concat2}`)
+//soal 25
+console.log(tantangan.repeat(2))
